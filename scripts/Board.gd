8fd4b384cac5a5594a409e07d8328ea4fe3e5810
solangebl <solange.blundi@gmail.com>
2020-04-12 18:05:22 +0200

    Changed Board to inherit from TileMap

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends TileMap
 class_name Board
 
 const BOARD_DIMENTIONS = Vector2(30,30)
@@ -7,15 +7,21 @@ var full_item_size = 330
 var scale_perc = 1
 var item_size = full_item_size
 
+var grid = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# calculate grid item sizes
 	var act_h = get_viewport().size.y
-	print(act_h)
 	item_size = float(act_h)/BOARD_DIMENTIONS.y
-	print(item_size)
 	scale_perc = (item_size)/full_item_size
-	print(scale_perc)
-	generate_tiles()
+	
+	# initialize the empty grid
+	for x in range(BOARD_DIMENTIONS.x):
+		grid.append([])
+		for y in range(BOARD_DIMENTIONS.y):
+			grid[x].append(null)
+	# TODO: move to game generate_tiles()
 
 func generate_tiles():
 	# TODO: generate game tiles, randomly save them in a stack to get them one by one
@@ -26,8 +32,31 @@ func generate_tiles():
 	add_child(initial_tile)
 	initial_tile.setPos(((BOARD_DIMENTIONS.x/2)-1)*item_size,((BOARD_DIMENTIONS.y/2)-1)*item_size)
 	
-func place_tile(x,y):
-	pass
+func place_tile(tile, x, y):
+	# 1. validate position is empty and within limits
+	print(is_vacant(x,y))
+	#if(is_vacant(x,y)):
+	# 2. add tile to the grid
+	grid[x][y] = "tile"
+	# 3. show tile on board
+	show_tile(tile, x, y)
+	
+func show_tile(tile, x, y):
+	tile.set_scale(Vector2(scale_perc,scale_perc))
+	add_child(tile)
+	tile.setPos(x*item_size,y*item_size)
+	
+func is_vacant(x,y):
+	return grid[x][y] != null
+	
+func _input(event):
+	# Mouse in viewport coordinates
+	if event is InputEventMouseButton:
+		# translate position to board
+		var new_pos = world_to_map((event.position))
+		var new_tile = load('res://scenes/tiles/InitialTile.tscn').instance()
+		place_tile(new_tile, new_pos.x, new_pos.y)
+		# place a tile at computed position 
 
 # Should decide if a tile can be placed at pos (x,y)
 
