9064fb5f97d6e7eb58f4f8ccd278eff262b54927
solangebl <solange.blundi@gmail.com>
2020-04-12 15:22:56 +0200

    Board con 30 piezas

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -3,26 +3,29 @@ class_name Board
 
 const BOARD_DIMENTIONS = Vector2(10,10)
 
-var full_h = 1080
 var full_item_size = 330
 var scale_perc = 1
+var item_size = full_item_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var act_h = get_viewport().size.y
-	scale_perc = float(act_h)/full_h
+	print(act_h)
+	item_size = float(act_h)/30
+	print(item_size)
+	scale_perc = (item_size)/full_item_size
+	print(scale_perc)
 	generate_tiles()
 
 func generate_tiles():
-	var item_size = full_item_size*scale_perc
 	# TODO: generate game tiles, randomly save them in a stack to get them one by one
 	# TEST
-	for x in range(BOARD_DIMENTIONS.x):
-		for y in range(BOARD_DIMENTIONS.y):
-			var new_tile = load('res://scenes/tiles/ChurchTile.tscn').instance()
-			new_tile.set_scale(Vector2(scale_perc,scale_perc))
-			add_child(new_tile)
-			new_tile.setPos(x*item_size,y*item_size)
+	for x in range(30):
+	#	for y in range(BOARD_DIMENTIONS.y):
+		var new_tile = load('res://scenes/tiles/ChurchTile.tscn').instance()
+		new_tile.set_scale(Vector2(scale_perc,scale_perc))
+		add_child(new_tile)
+		new_tile.setPos(0*item_size,x*item_size)
 
 # Should decide if a tile can be placed at pos (x,y)
 
