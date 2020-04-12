fc3111895a2f5cd8b6472db48a601855df9ec9b7
solangebl <solange.blundi@gmail.com>
2020-04-12 13:19:03 +0200

    Board: agrego escala para los tiles

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -3,6 +3,10 @@ class_name Board
 
 const BOARD_DIMENTIONS = Vector2(10,10)
 
+# TODO: calculate scale_perc based on screen size?
+var scale_perc = 0.5
+var item_size = 330*scale_perc
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -18,8 +22,9 @@ func generate_tiles():
 	for x in range(BOARD_DIMENTIONS.x):
 		for y in range(BOARD_DIMENTIONS.y):
 			var new_tile = load('res://scenes/tiles/ChurchTile.tscn').instance()
+			new_tile.set_scale(Vector2(scale_perc,scale_perc))
 			add_child(new_tile)
-			new_tile.setPos(x*330,y*330)
+			new_tile.setPos(x*item_size,y*item_size)
 
 # Should decide if a tile can be placed at pos (x,y)
 
