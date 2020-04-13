450114b20f10403dd92e3f046f2d33b294086e82
solangebl <solange.blundi@gmail.com>
2020-04-14 00:57:21 +0200

    Added liimt checks for the board

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -1,7 +1,7 @@
 extends TileMap
 class_name Board
 
-const BOARD_DIMENTIONS = Vector2(30,30)
+var board_dimentions = Vector2(30,30)
 
 var full_item_size = 330
 var scale_perc = 1
@@ -13,27 +13,29 @@ var grid = []
 func _ready():
 	# calculate grid item sizes
 	var act_h = get_viewport().size.y
-	item_size = float(act_h)/BOARD_DIMENTIONS.y
+	var act_w = get_viewport().size.x
+	item_size = float(act_h)/board_dimentions.y
+	var w_items = act_w/item_size
+	board_dimentions.x = w_items
 	set_cell_size(Vector2(item_size, item_size))
 	scale_perc = (item_size)/full_item_size
 	
 	# initialize the empty grid
-	for x in range(BOARD_DIMENTIONS.x):
+	for x in range(board_dimentions.x):
 		grid.append([])
-		for y in range(BOARD_DIMENTIONS.y):
+		for y in range(board_dimentions.y):
 			grid[x].append(null)
 	# TODO: move to game generate_tiles()
 
 func place_tile_center(tile):
-	var center = Vector2((((BOARD_DIMENTIONS.x/2)-1)*item_size), ((BOARD_DIMENTIONS.y/2)-1)*item_size)
+	var center = Vector2((((board_dimentions.x/2)-1)*item_size), ((board_dimentions.y/2)-1)*item_size)
 	#print(center)
 	place_tile(tile, center)
 	
 func place_tile(tile, pos: Vector2):
 	# 1. validate position is empty and within limits
 	var p = world_to_map(pos) 
-	print(is_vacant(p))
-	if(is_vacant(p)):
+	if(is_valid(p) and is_vacant(p)):
 		# 2. add tile to the grid
 		grid[p.x][p.y] = "tile"
 		# 3. show tile on board
@@ -49,11 +51,7 @@ func show_tile(tile, pos: Vector2):
 func is_vacant(pos):
 	return grid[pos.x][pos.y] != "tile"
 
-
-# Should decide if a tile can be placed at pos (x,y)
+func is_valid(pos):
+	return (pos.x >= 0 and pos.x <= board_dimentions.x-1) and (pos.y >= 0 and pos.y <= board_dimentions.y-1)
 
 # Should decide if a character can be placed at til
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
