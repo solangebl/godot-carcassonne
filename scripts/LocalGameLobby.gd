95c74e4aa31c8763cde01156cb483a908c562f86
Patricio Inzaghi <patricio.inzaghi@gmail.com>
2020-04-26 18:00:42 +0200

    menus changes

--- scripts/LocalGameLobby.gd
+++ scripts/LocalGameLobby.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,8 +8,8 @@ func _on_StartGame_gui_input(event):
 
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
 		
-		var player1 = Player.new($VBoxContainer/PlayerList/Player1.text)
-		var player2 = Player.new($VBoxContainer/PlayerList/Player2.text)
+		var player1 = Player.new($Menu/PlayerList/Player1.text)
+		var player2 = Player.new($Menu/PlayerList/Player2.text)
 		
 		Players.add(player1)
 		Players.add(player2)
--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,20 +1,10 @@
 extends MarginContainer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_NewGame_pressed():
-	get_tree().change_scene("res://scenes/menus/LocalGameLobby.tscn")
+func _on_LocalGameButton_gui_input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
+		get_tree().change_scene("res://scenes/menus/LocalGameLobby.tscn")
